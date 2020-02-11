4be438841f9345750fe12868967ae2142e557884
subhendu <bhattasubhendu@gmail.com>
2020-02-11 08:19:09 +0545

    installed voyager with order bread

--- routes/web.php
+++ routes/web.php
@@ -33,3 +33,8 @@ Route::get('paypal/checkout/{order}', 'PayPalController@getExpressCheckout')->na
 
 Route::get('paypal/checkout-success/{order}', 'PayPalController@getExpressCheckoutSuccess')->name('paypal.success');
 Route::get('paypal/checkout-cancel', 'PayPalController@cancelPage')->name('paypal.cancel');
+
+
+Route::group(['prefix' => 'admin'], function () {
+    Voyager::routes();
+});
