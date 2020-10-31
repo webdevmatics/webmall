651dbdef6b6a7d25a84d61dadeb968a6586a35ca
subhendu <bhattasubhendu@gmail.com>
2020-10-31 20:49:34 +0545

    more space in description table

--- database/migrations/2020_02_02_070326_create_products_table.php
+++ database/migrations/2020_02_02_070326_create_products_table.php
@@ -16,7 +16,7 @@ class CreateProductsTable extends Migration
         Schema::create('products', function (Blueprint $table) {
             $table->bigIncrements('id');
             $table->string('name');
-            $table->string('description');
+            $table->mediumText('description');
             $table->float('price');
             $table->string('cover_img')->nullable();
             $table->unsignedBigInteger('shop_id')->nullable();
