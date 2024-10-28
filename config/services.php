06d5976898a76ca07b44ad61a18db95d9fcf8c98
subhendu <bhattasubhendu@gmail.com>
2024-10-28 21:46:02 +0545

    Upgrade to laravel 11 latest

--- config/services.php
+++ config/services.php
@@ -30,4 +30,9 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'paypal' => [
+        'client_id' => env('PAYPAL_SANDBOX_CLIENT_ID'),
+        'secret' => env('PAYPAL_SANDBOX_CLIENT_SECRET'),
+    ],
+
 ];
